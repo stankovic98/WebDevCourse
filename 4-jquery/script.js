69b8ef79045befc6d8a7d5526106bebd13ecc9b7
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-05 21:42:15 +0200

    Dodani css i javascript paneli

--- 4-jquery/script.js
+++ 4-jquery/script.js
@@ -11,10 +11,23 @@ function main() {
 	$( ".toggleButton" ).click(function() {
 		$(this).toggleClass("active");
 		$( this ).removeClass("highlightedButton" );
+		
+		var panelId = $(this).attr("id") + "Panel";
+		$("#" + panelId).toggleClass("hidden");
+		var numberOfActivePanels = 4 - $(".hidden").length;
+		$(".panel").width( ($(window).width() / numberOfActivePanels ) - 10);
+		console.log($(".panel").width());
+		
 	});
 	
 	$("textarea").height($(window).height() - $("#header").height() -15);
-	$(".panel").width( ($(window).width() / 2 ) - 10);
+	//$(".panel").width( ($(window).width() / 2 ) - 10);
 	
 	$("iframe").contents().find("html").html($("#htmlPanel").val());
+	
+	$("#htmlPanel").on("change keyup paste", function() {
+		$("iframe").contents().find("html").html($("#htmlPanel").val());
+	});
+	
+	
 }
