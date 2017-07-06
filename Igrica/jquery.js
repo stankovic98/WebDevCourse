81c5a0f36120ddd17dda29d04cb0a456b61ecad2
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-06 09:03:10 +0200

    radi sve osim prikaza novih itema

--- Igrica/jquery.js
+++ Igrica/jquery.js
@@ -10,7 +10,26 @@ function main(){
 	var igraTraje = true;
 	
 	var igra = setInterval(makeNewObject, respawnItema);
-
+	setTimeout(prekiniIgru, trajanjeIgre, igra);
+	
+	setTimeout(prekidTimera, trajanjeIgre);
+	
+	function prekidTimera() {
+		$("#t").html("0"); 
+		igraTraje = false;
+	}
+	var int2 = setInterval(updateTime, 1000, int2);
+	function updateTime(interval) {
+		if(igraTraje) {
+			vrijeme--;
+			$("#t").html(vrijeme);
+			
+		}
+	}
+	
+	$("#button").click(function() {
+		location.reload();
+	});
 	
 	function makeNewObject() {
 		
@@ -19,7 +38,7 @@ function main(){
 		
 		$(".shape").click(function() {
 			counter++;
-			$(this).css("display", "none");
+			$(".shape").css("display", "none");
 			$("#i").html(counter);
 		});
 			
@@ -28,14 +47,45 @@ function main(){
 		
 		var color = getRandomColor();
 		
-		$(".shape").css({top: pos_Y, left: pos_X});
+		$(".shape").css({
+			top: pos_Y, 
+			left: pos_X
+		});
+		
+		$(".shape").css("background-color", color);
 		
 		var shape = Math.floor(Math.random() *3);
 		
+		switch (shape)
+		{
+			case 0:
+				var a = randomStranica();
+				$(".shape").height(a);
+				$(".shape").width(a);
+				break;
+			case 1:
+				var a = randomStranica();
+				var b = randomStranica();
+				$(".shape").height(a);
+				$(".shape").width(b);
+				break;
+			case 2:
+				var a = randomStranica();
+				$(".shape").height(a);
+				$(".shape").width(a);
+				$(".shape").css("border-radius", "50%");
+				break;
+		}
 		
-		
+	
 	}
 	
+	function prekiniIgru(interval) {
+		clearInterval(interval);
+		$("#gameOver").css("display","block");
+		$("#bodovi").html(counter);
+		
+	}
 	
 	
 	function getRandomColor() {
