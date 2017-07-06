1953b7a859e911c432d732035117ee54b6087458
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-06 19:00:37 +0200

    Zavrseni projekt u bootstrapu 4

--- 4-jquery/script.js
+++ 4-jquery/script.js
@@ -1,40 +1,6 @@
 function main() {
 	
-	function updateOutput() {
-		$("iframe").contents().find("html").html("<html><head><style>" + $("#cssPanel").val() 
-		+ "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
-		document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
-	}
-	
-	
-	$( ".toggleButton" ).hover(
-		function() {
-			$( this ).addClass("highlightedButton" );
-		} ,
-		function() {
-			$( this ).removeClass("highlightedButton" );
-		}
-	);
-	
-	$( ".toggleButton" ).click(function() {
-		$(this).toggleClass("active");
-		$( this ).removeClass("highlightedButton" );
-		
-		var panelId = $(this).attr("id") + "Panel";
-		$("#" + panelId).toggleClass("hidden");
-		var numberOfActivePanels = 4 - $(".hidden").length;
-		$(".panel").width( ($(window).width() / numberOfActivePanels ) - 10);
-		
-	});
-	
-	$("textarea").height($(window).height() - $("#header").height() -15);
-	
-	updateOutput();
-	
-	$("textarea").on("change keyup paste", function() {
-		updateOutput();
-	});
-	
+$("body").append("<div>asdasasd</div>");
 	
 	
 }
