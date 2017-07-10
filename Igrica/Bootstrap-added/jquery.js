89b76b137210ac35e2b0d4e29bbddb3f5673e0c9
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-10 19:29:18 +0200

    Dodana provjer ako je upisano ime

--- Igrica/Bootstrap-added/jquery.js
+++ Igrica/Bootstrap-added/jquery.js
@@ -1,12 +1,18 @@
-function main(){
-	
+function main() {
+	if ($("#name").val() == "") {
+		$("#mymodal").modal("toggle");
+		$("#cancle").click(function () {
+			$("#mymodal").modal("toggle");
+		});
+		return;
+	}
 	$("#pocetna").hide();
 	var user = $("#name").val()
 	$("#playerName").html(user);
 	$("#igra").toggle();
 	$(".progress-bar").width("100%");
 	var originalWidth = $(".progress-bar").width();
-	
+
 	var trajanjeIgre = 20 * 1000;
 	var respawnItema = 0.5 * 1000;
 	var trajanjeItema = 2 * 1000;
@@ -16,71 +22,70 @@ function main(){
 	var igraTraje = true;
 	var postotakPomicanjaItem = 0.11; //brojevi od 0 do 1
 	var postotakMinusBodovi = 0.22;
-	
+
 	var igra = setInterval(makeNewObject, respawnItema);
 	setTimeout(prekiniIgru, trajanjeIgre, igra);
-	
-	
-	
+
+
+
 	setTimeout(prekidTimera, trajanjeIgre);
-	
+
 	function prekidTimera() {
 		$(".progress-bar").css("width", "0");
 		igraTraje = false;
 	}
 	var int2 = setInterval(updateTime, 1000, int2);
 	function updateTime(interval) {
-		if(igraTraje) {
-			
+		if (igraTraje) {
 
-			
+
+
 			vrijeme--;
-			var postotak = Math.floor(( vrijeme/trajanjeIgreSEC*100));
+			var postotak = Math.floor((vrijeme / trajanjeIgreSEC * 100));
 			progressBar(postotak, $('.progress-bar'), originalWidth);
-			
-			
-			
+
+
+
 		}
 	}
-	
-	$("#restart").click(function() {
+
+	$("#restart").click(function () {
 		location.reload();
 	});
-	
+
 	function makeNewObject() {
-		
+
 		var animirano = false;
-		
-		var item = $( "<div class='shape'></div>" ).appendTo( "#room" );
+
+		var item = $("<div class='shape'></div>").appendTo("#room");
 		var minusBodovi = false;
 		item.css("display", "block");
 		item.bomba = false;
-		if(Math.random() < postotakMinusBodovi) {
-				item.css("border", "15px dotted red");
-				item.bomba = true;
-				animirano = true;
+		if (Math.random() < postotakMinusBodovi) {
+			item.css("border", "15px dotted red");
+			item.bomba = true;
+			animirano = true;
 		}
-		item.click(function() {
-			if(item.bomba) {
-				
+		item.click(function () {
+			if (item.bomba) {
+
 				counter--;
 				$("#bodovi").html(counter);
-				
+
 			} else {
 				counter++;
 				item.css("display", "none");
 				$("#bodovi").html(counter);
 			}
 		});
-			
-		
-		
-		
-		
-		var shape = Math.floor(Math.random() *3);
-		
-		switch (shape)
-		{
+
+
+
+
+
+		var shape = Math.floor(Math.random() * 3);
+
+		switch (shape) {
 			case 0:
 				var a = randomStranica();
 				item.height(a);
@@ -102,18 +107,18 @@ function main(){
 				item.addClass("animation-target3");
 				break;
 		}
-		
-		var pos_X = Math.floor(Math.random() * $("#room").width()-60);
-		var pos_Y = Math.floor(Math.random() * $("#room").height()-65);
-		
+
+		var pos_X = Math.floor(Math.random() * $("#room").width() - 60);
+		var pos_Y = Math.floor(Math.random() * $("#room").height() - 65);
+
 		var color = getRandomColor();
 		item.css("background-color", color);
-		
+
 		item.css({
-			top: pos_Y, 
+			top: pos_Y,
 			left: pos_X
 		});
-		
+
 		/*
 		item.hover(function() {
 			
@@ -150,34 +155,34 @@ function main(){
 		function destroyObject() {
 			item.hide();
 		}
-	
+
 		setTimeout(destroyObject, trajanjeItema)
-	
+
 	}
-	
+
 	function prekiniIgru(interval) {
 		clearInterval(interval);
 		$("#modal").modal("show");
 		$("#score").html(counter);
-		
+
 	}
-	
-	function progressBar(percent, $element,originalWidth) {
-		
+
+	function progressBar(percent, $element, originalWidth) {
+
 		var progressBarWidth = percent * originalWidth / 100;
 		$element.animate({ width: progressBarWidth }, 500);
 	}
-	
+
 	function getRandomColor() {
-	  var letters = '0123456789ABCDEF';
-	  var color = '#';
-	  for (var i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	  }
-	  return color;
+		var letters = '0123456789ABCDEF';
+		var color = '#';
+		for (var i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
 	}
-	
+
 	function randomStranica() {
-		return Math.floor(Math.random()*200)+40;
+		return Math.floor(Math.random() * 200) + 40;
 	}
 }
