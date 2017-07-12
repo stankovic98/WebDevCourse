6f4a2762230195ba1d6cc9b8e9b908c3ca5ad8e4
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-12 10:01:40 +0200

    dodan bootstrap

--- 8-mysql/index.php
+++ 8-mysql/index.php
@@ -71,7 +71,7 @@
 
                 $row = mysqli_fetch_array($result);
 
-                if(array_key_exists("id", $row)) {
+                if(isset($row)) {
                     $hashedPassword = md5(md5($row['id']).$_POST['password']);
 
                     if($hashedPassword == $row["password"]) {
@@ -81,38 +81,89 @@
                         }
                         header("Location: loggedinpage.php");
                     }
+                } else {
+                    $error = "That email/password combination could not be found.";
                 }
             }
         }
     }
 
 ?>
-<div id="error">
-    <?php echo $error; ?>
-</div>
 
-<form method="post">
 
-    <input type="email" name="email" placeholder="Your Email">
 
-    <input type="password" name="password" placeholder="Password">
 
-    <input type="checkbox" name="stayLoggedIn" value=1 >
 
-    <input type="hidden" name="signUp" value="1">
 
-    <input type="submit" name="submit" value="Sign Up!">
-</form>
 
-<form method="post">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <input type="email" name="email" placeholder="Your Email">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <style>
 
-    <input type="password" name="password" placeholder="Password">
+        .container {
+            text-align: center;
+            width: 400px;
+        }
 
-    <input type="checkbox" name="stayLoggedIn" value=1 >
+    </style>
+  </head>
+  <body>
 
-    <input type="hidden" name="signUp" value="0">
+    <div class="container">
+        <h1>Secret Diary</h1>
 
-    <input type="submit" name="submit" value="Log In">
-</form>
+        <div id="error">
+            <?php echo $error; ?>
+        </div>
+        <form method="post">
+            <div class="form-group">
+                <input type="email" class="form-control"name="email" placeholder="Your Email">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control"name="password" placeholder="Password">
+            </div>
+            <div class="form-group">
+                Stay logged in:
+                <input type="checkbox" name="stayLoggedIn" value=1 >
+            </div>
+            <div class="form-group">
+                <input type="hidden" name="signUp" value="1">
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-success"name="submit" value="Sign Up!">
+            </div>
+        </form>
+
+        <form method="post">
+            
+            <div class="form-group">
+                <input type="email" class="form-control"name="email" placeholder="Your Email">
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control"name="password" placeholder="Password">
+            </div>
+            <div class="checkbox">
+                <lable>Stay logged in:</lable>
+                <input type="checkbox" name="stayLoggedIn" value=1 >
+            </div>
+            <div class="form-group">
+                <input type="hidden" name="signUp" value="0">
+                <input type="submit" class="btn btn-success"name="submit" value="Log In">
+            </div>
+        </form>
+    </div>
+    
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
