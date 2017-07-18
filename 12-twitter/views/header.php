030939ce27d7654122d2e36b0afa098184b203da
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-18 14:08:20 +0200

    Napravio navbar

--- 12-twitter/views/header.php
+++ 12-twitter/views/header.php
@@ -9,3 +9,23 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
   </head>
   <body>
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+          <form class="form-inline pull-xs-right">
+      <button class="navbar-toggler navbar-toggler-right btn-success-outline" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> Login/Sign Up
+    
+  </button>
+  </form>
+  <ul class="nav">
+  <a class="navbar-brand" href="http://antonio.stankovic.com.stackstaging.com/contents/12-twitter/">Twitter</a>
+  <li class="nav-item">
+    <a class="nav-link active" href="?page=timeline">Your timeline</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="?page=yourtweets">Your tweets</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="?page=publicprofiles">Public Profiles</a>
+  </li>
+</ul>
+
+</nav>
