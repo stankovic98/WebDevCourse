b54861a8c399cbae6f3453037654efb4529abb6c
Antonio Stankovic <antoniostankovic123@gmail.com>
2017-07-20 00:36:47 +0200

    Validacija unosa

--- 12-twitter/actions.php
+++ 12-twitter/actions.php
@@ -2,7 +2,20 @@
 
     include("functions.php");
     if($_GET["action"] == "loginSingUp") {
-        var_dump($_POST);
+        $error = "";
+        if(!$_POST["email"]) {
+            $error .= "You need to enter email";
+        }
+        if (!$_POST["password"]) {
+            $error .= "You need to enter password";
+        }
+        if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+            $error .= "Invalid email format"; 
+        }
+        echo $error;
+        if($_POST["loginActive"] == "0") {
+
+        }
     }
 
 ?>
